3a3c26bd67115ac10b79cf2242b438a9f6ac33c5
alessio tommasi <alessio.tommasi.lavoro@gmail.com>
2026-07-03 08:42:01 +0200

    fix space error in search live

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,11 @@ return {
     
     -- Gestione della modalità INSERIMENTO dentro il menu di Telescope
     opts.defaults.mappings.i = vim.tbl_deep_extend("force", opts.defaults.mappings.i or {}, {
-      ["<Space>"] = actions.select_default, -- Spazio apre il file
+      --["<Space>"] = actions.select_default, -- Spazio apre il file
+      --opts Ctrl + Spazio inserisce un carattere spazio reale nel prompt
+      ["<C-Space>"] = function()
+        vim.api.nvim_put({ " " }, "c", true, true)
+      end,
     })
     
     -- Gestione della modalità NORMALE dentro il menu di Telescope
